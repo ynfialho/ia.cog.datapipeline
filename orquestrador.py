e8b5c8f44b70f101a0c3e4940ca51a52caeb72a2
unknown <ynfialho@gmail.com>
2018-07-30 03:10:58 -0300

    adjustment pip install

--- orquestrador.py
+++ orquestrador.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
-import load_stg
-import load_tables
-
+import sys
 
 def main():
+
+    import load_stg
+    import load_tables
+
     os.system('python3.6 {}'.format(load_stg.__file__))
     os.system('python3.6 {}'.format(load_tables.__file__))
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        os.system('pip3.6 install -r requirements.txt -t .')
+        main()
+    except:
+        sys.exit('Error na instalação do requirements.')
